4096229b46442247fcfe42f50c311f3a4460ec08
Adarsha D Shetty <136900544+adarshadshetty@users.noreply.github.com>
2024-07-12 19:04:59 +0530

    Update main.sh

--- Project2/main.sh
+++ Project2/main.sh
@@ -2,7 +2,7 @@
 echo "Check disk usage in Linux system"
 disk_size=`df -h|grep "/dev/xvda1"|awk '{print $5}'|cut -d '%' -f 1`
 echo "$disk_size% of disk is filled"
-if [ $disk_size -gt 80 ];
+if [ $disk_size -gt 10 ];
 then
         echo "disk is utilized more than 80%, expand disk or delete files soon"
 else
