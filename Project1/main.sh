cfb16e993b32b810469be63c48860e067cd963cc
Adarsha D Shetty <136900544+adarshadshetty@users.noreply.github.com>
2024-07-12 08:03:49 +0530

    Update main.sh

--- Project1/main.sh
+++ Project1/main.sh
@@ -1,1 +1,15 @@
-
+#!/bin/bash
+echo "Script to install git"
+echo "Installation started"
+if [ "$(uname)" == "Linux" ];
+then    
+        echo "This is linux box, installing git"
+        yum install git -y
+elif [ "$(uname)" == "Darwin" ];
+then    
+        echo "This is not linux box"
+        echo "This is MacOS"
+        brew install git
+else    
+        echo "not installing"
+fi
