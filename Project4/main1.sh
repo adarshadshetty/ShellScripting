e1dd9deaa020c1158ecf69080c7154cf7b47092e
Adarsha D Shetty <136900544+adarshadshetty@users.noreply.github.com>
2024-07-13 09:49:06 +0530

    Update main1.sh

--- Project4/main1.sh
+++ Project4/main1.sh
@@ -1,1 +1,12 @@
 
+#!/bin/bash
+echo "This script delete files which are older than 30 days"
+path="$1"
+echo $path
+find $path -mtime +1 -delete
+if [[ $? -eq 0]];
+then
+        echo "Files are successfully deleted"
+else
+        echo "deletion was having some issue"
+fi
