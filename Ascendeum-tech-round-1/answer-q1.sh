3f61f7e75f0f2441e9ef4a77a64b6d97351e09ba
Adarsha D Shetty <136900544+adarshadshetty@users.noreply.github.com>
2024-07-17 16:54:28 +0530

    Create answer-q1.sh

---
+++ Ascendeum-tech-round-1/answer-q1.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# Reverse the strinear
+#
+read -p "Type the string: " string
+
+len=`echo ${#string}`
+reverse=""
+
+i=$(($len - 1));
+while [ $i -ge 0 ]
+do
+        reverse+=`echo ${string:$i:1}`
+        let i-=1
+done
+
+echo "Reverse string :$reverse"
