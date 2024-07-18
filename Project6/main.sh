daa3e9c8b5e95cdb44a36ff9533b211643b1f01c
Adarsha D Shetty <136900544+adarshadshetty@users.noreply.github.com>
2024-07-18 14:38:22 +0530

    prometheus installation script for linux

--- Project6/main.sh
+++ Project6/main.sh
@@ -1,1 +1,12 @@
-
+#!/bin/bash
+echo "Download the prometheus binaries"
+if [ -e /home/ec2-user/prometheus-2.32.1.linux-amd64.tar.gz ];
+then
+        echo "File is already existiong so no need to download "
+        tar -zxvf /home/ec2-user/prometheus-2.32.1.linux-amd64.tar.gz
+else
+        echo "Binary does not exists in system first we need to download it"
+        wget https://github.com/prometheus/prometheus/releases/download/v2.32.1/prometheus-2.32.1.linux-amd64.tar.gz
+        tar -zxvf prometheus-2.32.1.linux-amd64.tar.gz
+        echo "File has been extracted , you can start prometheus"
+fi
