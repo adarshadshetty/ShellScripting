b98a7f952b6151e92401b3441e53f62b71f64b4e
Adarsha D Shetty <136900544+adarshadshetty@users.noreply.github.com>
2024-07-19 17:40:04 +0530

    Update main.sh

--- Project8/main.sh
+++ Project8/main.sh
@@ -1,1 +1,12 @@
+#!/bin/bash
+echo "====Status check docker service===="
+status="`sudo systemctl status docker | awk 'NR==3 {print}'|cut -d ':' -f 2 |cut -d '(' -f 1
+`"
+echo $status
+if [ $status = "active" ];
+then
+        echo "service is running fine......."
+else
+        echo "service is not running........"
+fi
 
