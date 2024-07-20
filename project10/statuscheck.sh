625f2d71ed4f7fb9350acf55bd40815f8b91144c
Adarsha D Shetty <136900544+adarshadshetty@users.noreply.github.com>
2024-07-20 13:45:16 +0530

    Update statuscheck.sh

--- project10/statuscheck.sh
+++ project10/statuscheck.sh
@@ -1,1 +1,10 @@
+#!/bin/bash
+echo " Welcome to service status check script "
+read -p "Enter the service name to check its status: " service_name
+if [ -z $service_name ];
+then
+        echo " Please eneter a valid service name "
+else
+        sudo systemctl status $service_name
+fi
 
