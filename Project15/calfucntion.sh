847c708d6fb0c8120aa71a630ae69c21cf37b657
Adarsha D Shetty <136900544+adarshadshetty@users.noreply.github.com>
2024-07-20 18:10:55 +0530

    Create calfucntion.sh

---
+++ Project15/calfucntion.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+clear
+echo "--------------------------------"
+echo "-------welcome to calculator----"
+echo "--------------------------------"
+read_input()
+{
+        read -p "Enter first number: " num1
+        read -p "Enter second number: " num2
+}
+
+echo -e "[a]ddition\n[b]Subtraction\n[c]Multiplication\n[d]Division\n"
+read -p "Enter your choice: " choice
+case $choice in
+   [aA])
+
+           result=$((num1+num2))
+           echo "The result for your choice is: $result"
+           ;;
+   [bB])
+           result=$((num1-num2))
+           echo "The result for your choice is: $result"
+           ;;
+   [cC])
+           result=$((num1*num2))
+           echo "The result for your choice is: $result"
+           ;;
+   [dD])
+           result=$((num1/num2))
+           echo "The result for your choice is: $result"
+           ;;
+   *)
+           echo "Wrong choice"
+           ;;
+esac
