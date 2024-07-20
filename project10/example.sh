4d739041aba9c9a225f1296131bda96b6ad0361a
Adarsha D Shetty <136900544+adarshadshetty@users.noreply.github.com>
2024-07-20 13:56:38 +0530

    Delete project10 directory

--- project10/example.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-read -p "What is your name: " name
-read -p "Enter the password: " -s password
-echo $name
-echo $password
-read -p "It will timeout in 3 seconds: " -t 10
--- project10/while.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-echo "While demo........"
-while read -r line;
-do
-        echo "$line"
-done < test.txt
