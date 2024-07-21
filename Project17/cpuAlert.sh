cc773025b9f2fedb6d2323fc1e444440092bf2b9
Adarsha D Shetty <136900544+adarshadshetty@users.noreply.github.com>
2024-07-21 08:16:00 +0530

    Create cpuAlert.sh

---
+++ Project17/cpuAlert.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo "CPU Load average value check"
+load=` top -bn1 | grep load | awk '{printf "%.2f%%\t\t\n", $(NF-2)}'|cut -d '%' -f1 `
+echo $load
+if [[ ${load%.*} > 1 ]];
+then
+        echo "CPU load is very high:$load"
+else
+        echo "Load is normal"
+fi
